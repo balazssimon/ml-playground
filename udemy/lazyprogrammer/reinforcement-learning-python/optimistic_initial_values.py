271aaf42065a2f2494fc767c1d30330eee1367bc
Balazs Simon <balazssimon@gmail.com>
2018-08-02 23:08:10 +0200

    Reinforcement learning in Python

---
+++ udemy/lazyprogrammer/reinforcement-learning-python/optimistic_initial_values.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from comparing_epsilons import run_experiment as run_experiment_eps
+
+class bandit:
+    def __init__(self, m, upper_limit):
+        self.m = m
+        self.mean = upper_limit
+        self.n = 1
+
+    def pull(self):
+        return np.random.randn() + self.m
+
+    def update(self, x):
+        self.n += 1
+        self.mean = (1 - 1.0/self.n)*self.mean + 1.0/self.n*x
+
+
+def run_experiment(m1, m2, m3, n, upper_limit=10):
+    bandits = [bandit(m1, upper_limit), bandit(m2, upper_limit), bandit(m3, upper_limit)]
+
+    data = np.empty(n)
+
+    for i in range(n):
+        # optimistic initial values
+        j = np.argmax([b.mean for b in bandits])
+        x = bandits[j].pull()
+        bandits[j].update(x)
+
+        # for the plot
+        data[i] = x
+    cumulative_average = np.cumsum(data) / (np.arange(n) + 1)
+
+    # plot moving average ctr
+    plt.plot(cumulative_average)
+    plt.plot(np.ones(n)*m1)
+    plt.plot(np.ones(n)*m2)
+    plt.plot(np.ones(n)*m3)
+    plt.xscale('log')
+    plt.show()
+
+    for b in bandits:
+        print(b.mean)
+
+    return cumulative_average
+
+
+
+if __name__ == '__main__':
+    c_1 = run_experiment_eps(1.0, 2.0, 3.0, 0.1, 100000)
+    oiv = run_experiment(1.0, 2.0, 3.0, 100000)
+
+    # log scale plot
+    plt.plot(c_1, label='eps = 0.1')
+    plt.plot(oiv, label='optimistic')
+    plt.legend()
+    plt.xscale('log')
+    plt.show()
+
+
+    # linear plot
+    plt.plot(c_1, label='eps = 0.1')
+    plt.plot(oiv, label='optimistic')
+    plt.legend()
+    plt.show()
+
